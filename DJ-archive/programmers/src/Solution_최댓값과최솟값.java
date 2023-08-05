2946fce33aafef47990ca8c08ddedc443d3e6bc2
윤다정 <ydajeong7@gmail.com>
2023-08-05 23:33:30 +0900

    프로그래머스 - 최댓값과 최솟값

---
+++ DJ-archive/programmers/src/Solution_최댓값과최솟값.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Solution_최댓값과최솟값 {
+
+    public static void main(String[] args) {
+        String s = "-1 -2 -3 -4";
+        System.out.println(Solution(s));
+    }
+
+    private static String Solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (String ss : s.split(" ")) {
+            int i = Integer.parseInt(ss);
+            if (i > max) max = i;
+            if (i < min) min = i;
+        }
+        answer.append(min).append(" ").append(max);
+        return answer.toString();
+    }
+
+}

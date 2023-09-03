aa4326ff8c27591597c689f948774585da6d2a72
DJ-archive <ydajeong7@gmail.com>
2023-09-04 00:03:50 +0900

    코드트리 - 동전 거슬러주기 (수정)

--- DJ-archive/codetree/src/dp/Main_동전거슬러주기.java
+++ DJ-archive/codetree/src/dp/Main_동전거슬러주기.java
@@ -10,7 +10,7 @@ public class Main_동전거슬러주기 {
 
     // 지금까지 선택한 동전의 합이 i라 했을 때, 가능한 최소 동전의 개수
     // 합 i를 만들기 위해 마지막으로 사용한 동전이 j번째 동전(coin[j])인 경우
-    // dp[i] = min (dp[i-coin[j]) + 1 (i<=j<=n, i>=coin[j])
+    // dp[i] = min (dp[i-coin[j]+1) (1<=j<=n, i>=coin[j])
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -30,12 +30,12 @@ public class Main_동전거슬러주기 {
             for (int j = 1; j <= N; j++) {
                 if (i-coins[j]>=0) {
                     if (dp[i-coins[j]] == Integer.MAX_VALUE) continue; // ***
-                    dp[i] = Math.min(dp[i], dp[i-coins[j]]) + 1;
+                    dp[i] = Math.min(dp[i], dp[i-coins[j]]+1);
                 }
             }
         }
 
-        if (dp[N] == Integer.MIN_VALUE) dp[N] = -1;
-        System.out.println(dp[N]);
+        if (dp[M] == Integer.MAX_VALUE) dp[M] = -1;
+        System.out.println(dp[M]);
     }
 }
